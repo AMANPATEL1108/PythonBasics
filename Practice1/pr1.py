668e81fe6d3967be343cb3d5206718f6d3d00d5c
AMANPATEL1108 <amanpatel2482@gmail.com>
2025-08-20 08:32:49 +0530

    Ok

--- Practice1/pr1.py
+++ Practice1/pr1.py
@@ -91,12 +91,12 @@
 
 # print(random.randint(1, 10))
 # ------------------------------------------------------------------------------------
-with open("sample.txt", "w") as f:
-        f.write("Hello, file!")
+# with open("sample.txt", "w") as f:
+#         f.write("Hello, file!")
 
-with open("sample.txt", "r") as f:
-        content = f.read()
-        print(content)
+# with open("sample.txt", "r") as f:
+#         content = f.read()
+#         print(content)
 # ------------------------------------------------------------------------------------
 class Person:
         def __init__(self, name):
